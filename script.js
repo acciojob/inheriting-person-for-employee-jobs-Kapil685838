f8264ca3a5861affffa6179c57863613a2a3d078
Kapil Chaudhary <108571992+Kapil685838@users.noreply.github.com>
2023-04-10 10:46:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
-// complete this js code
+// Define the Person constructor function
 function Person(name, age) {
-	this._name = name;
-	this._age = age;
+  this.name = name;
+  this.age = age;
 }
 
+// Add the greet method to the Person prototype
 Person.prototype.greet = function() {
-	console.log(`Hello, my name is ${this._name}, I am ${this._age} years old.`)
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
 }
 
+// Define the Employee constructor function
 function Employee(name, age, jobTitle) {
-	Person.call(this, name, age);
-	this._jobTitle = jobTitle;
+  Person.call(this, name, age); // call the Person constructor with `this` as the context
+  this.jobTitle = jobTitle;
 }
 
 // Inherit the Person prototype in the Employee prototype
 Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 
+// Add the jobGreet method to the Employee prototype
 Employee.prototype.jobGreet = function() {
-	console.log(`Hello, my name is ${this._name}, I am ${this._age} years old, and my job title is ${this._jobTitle}.`);
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 }
+
 
 // Do not change code below this line
 window.Person = Person;
