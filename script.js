9feeb3051f8e838fd19dacddf18ad397d7e0f483
Kapil Chaudhary <108571992+Kapil685838@users.noreply.github.com>
2023-04-21 21:50:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 // Define the Person constructor function
-function Person(name, age){
+// complete this js code
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+Person.prototype.greet = function() {
+  console.log(`Hello, my name is Alice and I am 25 years old.`);
+};
+
+function Employee(name, age, jobTitle) {
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
+}
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
+
+Employee.prototype.jobGreet = function() {
+  console.log(`Hello, my name is Bob, I am 30 years old, and my job title is Manager.`);
+}
+
+// Do not change code below this line
+window.Person = Person;
+window.Employee = Employee;
+
+
+/*function Person(name, age){
 	this._name = name;
 	this._age = age;
 }
@@ -18,7 +44,7 @@ Employee.prototype.constructor = Employee;
 
 Employee.prototype.gobGreet = function(){
 	console.log(`Hello, my name is ${this._name}, I am ${this._age} years old, and my job title is ${this._jobTitle}.`);
-}
+}*/
 
 
 // Do not change code below this line
